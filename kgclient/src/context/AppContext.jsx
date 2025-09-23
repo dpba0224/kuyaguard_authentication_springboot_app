6f759239e654bcaac3ab38ba8ac94a761148b653
David Polo Abrugena <dpbabrugena@gmail.com>
2025-09-23 22:36:53 +0800

    2025-09-23 v.01: First commit

---
+++ kgclient/src/context/AppContext.jsx
@@ -0,0 +1,66 @@
+import { createContext, useEffect, useState } from "react";
+import { AppConstants } from "../util/constants";
+import {toast} from "react-toastify";
+import axios from "axios";
+
+// Create a context
+export const AppContext = createContext();
+
+// Create a provider
+export const AppContextProvider = (props) => {
+
+    axios.defaults.withCredentials = true;
+
+    const backendURL = AppConstants.BACKEND_URL;
+
+    // Creation of State
+    const[isLoggedIn, setIsLoggedIn] = useState(false);
+    const[userData, setUserData] = useState(false);
+
+    const getUserData = async() => {
+        try{
+            const response = await axios.get(backendURL + "/profile");
+
+            if(response.status === 200){
+                setUserData(response.data);
+            }
+            else{
+                toast.error("Unable to retrieve the user's profile.");
+            }
+        }
+        catch(error){
+            toast.error(error.message);
+        }
+    }
+
+    const getAuthState = async () => {
+        try {
+            const response = await axios.get(backendURL+"/is-authenticated");
+            if (response.status === 200 && response.data === true) {
+                setIsLoggedIn(true);
+                await getUserData();
+            } else {
+                setIsLoggedIn(false);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getAuthState();
+    }, []);
+    
+    const contextValue = {
+        backendURL,
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData,
+        getUserData,
+    }
+    
+    return(
+        <AppContext.Provider value={contextValue}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
